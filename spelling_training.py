56eada47546648820f5d2e1a200946877ec9e796
AzinT <ataqizad@uwaterloo.ca>
2020-02-16 08:57:19 -0500

    Azin is a dumbass

--- spelling_training.py
+++ spelling_training.py
@@ -5,10 +5,23 @@ import os
 path = "spelling-model-weighted.txt"
 assert os.path.isfile(path)
 spelling = Spelling(path=path)
-
 MOCKDATA = "hi i dont spel"
 test = TextBlob(MOCKDATA)
-print(test.sentences)
-for word in test.sentences[0].words:
-    print(word)
+test1 = test.replace('dont',"don't")
+test1 = test1.replace('doesnt',"doesn't")
+test1 = test1.replace('didnt',"didn't")
+test1 = test1.replace('wont',"won't")
+test1 = test1.replace('wouldve',"would've")
+test1 = test1.replace('cant',"can't")
+test1 = test1.replace('couldnt',"couldn't")
+test1 = test1.replace('couldve',"could've")
+test1 = test1.replace('shouldnt',"shouldn't")
+test1 = test1.replace('shoulve',"shouldn've")
+test1 = test1.replace('mightve',"might've")
+test1 = test1.replace('havent',"haven't")
+test1 = test1.replace('lets',"let's")
+
+print(test1)
+for word in test1.words:
+    print(spelling.suggest(word))
 
